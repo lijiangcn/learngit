996487aec5e0b4b57a0584af5e7051814b17d062
lijiangcn <lijiangcn@126.com>
2021-06-05 21:11:53 +0800

    mainwindow

--- mainwindow.py
+++ mainwindow.py
@@ -1,16 +1,22 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QDesktopWidget,QMainWindow
-from PyQt5.QtGui import QIcon
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import *
 
 class mainWindow(QMainWindow):
     def __init__(self,parent=None):
         super(mainWindow,self).__init__(parent)
-        self.resize(400,200)
+        self.resize(800,500)
         #实例化建状态栏
         self.status=self.statusBar()
         #4000 is the duration of message
         self.status.showMessage('this is a remark',4000)
         self.setWindowTitle('example of pyqt')
+        baseWidget=QWidget()
+        self.setCentralWidget(baseWidget)
+        btnClose=QPushButton(baseWidget)
+        btnClose.setText('close')
+        btnClose.setGeometry(100,90,75,23)
+        btnClose.clicked.connect(self.onButtonClick)
 
     def center(self):
         #get the size of screen
@@ -18,8 +24,12 @@ class mainWindow(QMainWindow):
         size=self.geometry()
         #move the window to the center of the screen
         self.move((screen.width()-size.width())/2,(screen.height()-size.height())/2)
-
-
+ 
+    def onButtonClick(self):
+        sender=self.sender()
+        print(sender.text+'pressed')
+        qApp=QApplication.instance()
+        qApp.quit()
 
 if __name__=='__main__':
     app=QApplication(sys.argv)
